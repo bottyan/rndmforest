4659bb029389e0cda26e88c033792019c322d2d0
bottyan <bottyan@gmail.com>
2021-10-22 11:14:09 +0200

    Update dialogity.vue
    Update site layout

--- dialogitycom/layouts/dialogity.vue
+++ dialogitycom/layouts/dialogity.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <div>
+    <div class="header">
+      <div class="menu">
         <NuxtLink to="/">Home</NuxtLink>
         <NuxtLink to="/live_chat">Live Chat</NuxtLink>
         <NuxtLink to="/chat_automation">Chatbots</NuxtLink>
         <NuxtLink to="/ai_sales_assistant">AI Sales Assistant</NuxtLink>
+      </div>
     </div>
     <Nuxt />
+    <div class="footer">
+      <NuxtLink to="/terms_and_conditions">Terms and conditions</NuxtLink>
+    </div>
   </div>
 </template>
