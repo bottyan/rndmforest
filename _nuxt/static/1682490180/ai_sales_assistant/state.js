ef1fe31e7d89fae9d276ee39921220e0ebfa02fb
bottyan <bottyan@users.noreply.github.com>
2023-04-28 08:44:46 +0000

    deploy: b9e1af474eddce409344329bb05039498558bb6b

--- _nuxt/static/1682490180/ai_sales_assistant/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Frndmforest\u002F_nuxt\u002Fstatic\u002F1682490180",layout:"dialogity",error:a,serverRendered:true,routePath:"\u002Fai_sales_assistant",config:{_app:{basePath:"\u002Frndmforest\u002F",assetsPath:"\u002Frndmforest\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- _nuxt/static/1682490180/live_chat/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Frndmforest\u002F_nuxt\u002Fstatic\u002F1682490180",layout:"dialogity",error:a,serverRendered:true,routePath:"\u002Flive_chat",config:{_app:{basePath:"\u002Frndmforest\u002F",assetsPath:"\u002Frndmforest\u002F_nuxt\u002F",cdnURL:a}}}}(null));
