5fc20fa5c99087e9d2288e988ac95086f55d2288
bottyan <bottyan@users.noreply.github.com>
2023-04-28 08:50:41 +0000

    deploy: 692904f5242b8837ccec8703d784452a56bb9f33

---
+++ _nuxt/static/1682671840/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fhu\u002Fpricing","\u002Fai_sales_assistant","\u002Fchat_automation","\u002Flive_chat","\u002Fpricing","\u002Fprivacy_policy","\u002Fterms_of_service","\u002Fhu\u002Fprivacy-policy","\u002Fprivacy-policy","\u002Fwordpress-plugin","\u002Fhu"]})
