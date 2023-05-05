ed26db6cba84cf36531cda9246e95ecc9cb67671
bottyan <bottyan@gmail.com>
2023-05-05 05:08:49 +0000

    Addign Google Fonts

--- rndmforesteu/nuxt.config.ts
+++ rndmforesteu/nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
-        '@nuxt/content'
+        '@nuxt/content',
+        '@nuxtjs/google-fonts',
     ],
     content: {
         // https://content.nuxtjs.org/api/configuration
@@ -9,5 +10,23 @@ export default defineNuxtConfig({
     ssr: false,
     nitro: {
         preset: 'service-worker'
+    },
+    googleFonts: {
+        families: {
+            // https://themeisle.com/blog/best-google-fonts/#gref
+            Roboto: true,
+            'Josefin+Sans': true,
+            'Merriweather': true,
+            Lora: true,
+            'Playfair Display': true,
+            'Indie Flower': true,
+            'Open Sans': true,
+            Anton: true,
+            Lato: [100, 300],
+            Raleway: {
+                wght: [100, 400],
+                ital: [100]
+            },
+        }
     }
 })
