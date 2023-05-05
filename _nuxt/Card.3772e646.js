f2d88c83889e1b9626a2dfbf1a1aa4d926270b77
bottyan <bottyan@users.noreply.github.com>
2023-05-05 07:39:55 +0000

    deploy: 46b2a06db900b45cc33d4266cf88f21ea17a6439

---
+++ _nuxt/Card.3772e646.js
@@ -0,0 +1,1 @@
+import{a as e,o,b as a,r as s}from"./entry.9294fcf0.js";const t={},c={class:"p-6 border bg-white dark:bg-black dark:border-gray-700 rounded"};function d(r,n){return o(),a("div",c,[s(r.$slots,"default")])}const _=e(t,[["render",d]]);export{_ as default};
---
+++ _nuxt/TheTitle.fa7893b1.js
@@ -0,0 +1,1 @@
+import o from"./ContentSlot.1667b8ed.js";import{a as s,o as n,b as c,f as r}from"./entry.9294fcf0.js";import"./utils.8441e260.js";import"./preview.07fdaa47.js";const a={},_={class:"text-4xl"};function l(e,f){const t=o;return n(),c("h1",_,[r(t,{use:e.$slots.default,unwrap:"p"},null,8,["use"])])}const d=s(a,[["render",l]]);export{d as default};
