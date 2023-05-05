d62e1c280ee45866bc37e763640eb1b9c9abbce0
bottyan <bottyan@gmail.com>
2023-05-05 07:58:06 +0000

    experimentation with navigation

--- rndmforesteu/pages/[...slug].vue
+++ rndmforesteu/pages/[...slug].vue
@@ -1,6 +1,36 @@
+<script setup>
+  const catsQuery = queryContent('/pub/')
+  /*
+  // If you'd prefer to pass along raw `QueryBuilderParams`:
+  const catsQuery = {
+    where: [
+      { _path: /^\/cats/ },
+    ],
+  }
+  */
+  // TODO: document driven example: https://dev.to/ceceliacreates/document-driven-mode-in-nuxt-content-v2-42em
+  // TODO: fix navigation
+</script>
+
 <template>
     <main>
+      LIST
+      <ContentList path="/pub" v-slot="{ list }">
+        <div v-for="article in list" :key="article._path">
+          <h2>{{ article.title }}</h2>
+        </div>
+      </ContentList>
+      NAVIGATION
+      <ContentNavigation v-slot="{ navigation }" :query="catsQuery">
+        <ul>
+          <li v-for="link of navigation" :key="link._path">
+            <NuxtLink :to="link._path">{{ link.title }}</NuxtLink>
+          </li>
+        </ul>
+      </ContentNavigation>
+      REST
       <ContentDoc />
     </main>
 </template>
+
   
--- rndmforesteu/pages/pub/[...slug].vue
+++ rndmforesteu/pages/pub/[...slug].vue
@@ -2,8 +2,14 @@
     <main>
       <h1>Page</h1>
       <ContentDoc>
-        <template #not-found> <h1>Document not found</h1> </template>
+        <template #not-found> <h1 class="red">Document not found</h1> </template>
       </ContentDoc>
     </main>
 </template>
+
+<style lang="scss">
+  .red {
+    color: red;
+  }
+</style>
   
