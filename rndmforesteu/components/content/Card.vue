dddb5156cb14681e382fe4cdf0aab83ff5dee7b6
Bottyan Nemeth <bottyan@gmail.com>
2023-05-13 14:16:51 +0200

    WiP main page content and design

--- rndmforesteu/components/content/Card.vue
+++ rndmforesteu/components/content/Card.vue
@@ -8,5 +8,8 @@
 <style scoped>
 .card {
     flex-grow:1;
+    background-color: gray;
+    border-radius: 2rem;
+    padding: 2rem;
 }
 </style>
--- rndmforesteu/components/content/Cards.vue
+++ rndmforesteu/components/content/Cards.vue
@@ -12,6 +12,8 @@
     justify-content: center;
     align-items: normal;
     align-content: normal;
+    gap: 1rem;
+    padding: 1rem;
 
     margin-left: auto;
     margin-right: auto;
@@ -20,6 +22,7 @@
 @media only screen and (max-width: 600px) {
     .cards {
         flex-direction: column;
+        padding: 0.5rem;
     }
 }
 </style>
