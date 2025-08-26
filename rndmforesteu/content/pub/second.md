80f13c1066b0eef45b8c5938c802f4ecda87cea3
Bottyan Nemeth <bottyan@gmail.com>
2025-08-26 08:06:29 +0200

    Locally working version.

---
+++ rndmforesteu/content/pub/second.md
@@ -0,0 +1,22 @@
+---
+title: 'Title of the page'
+description: 'meta description of the page'
+label: 'test'
+---
+
+::description
+This is an important text
+::
+
+::paragraph
+This is a longer text, which is a paragraph.
+
+:::block
+And the firs letter should be bigger, but I don't see it, why?
+:::
+
+:::block
+This is an other *something* in an other block so the texts should be separated with an empty space.
+[link](https://google.com)
+:::
+::
--- rndmforesteu/pages/pub/[...slug].vue
+++ rndmforesteu/pages/pub/[...slug].vue
@@ -1,7 +1,17 @@
 <template>
     <main>
-      <ContentDoc>
+      <!--ContentDoc>
         <template #not-found> <h1 class="red">Document not found</h1> </template>
+      </ContentDoc-->
+      <ContentDoc v-slot="{ doc }">
+        <div v-if="doc">
+          <article>
+            <h1>{{ doc.title }}</h1>
+            <h2>{{ doc.label }}</h2>
+            <ContentRenderer :value="doc" />
+          </article>
+        </div>
+        <div v-else> <h1 class="red">Document not found</h1></div>
       </ContentDoc>
     </main>
 </template>
